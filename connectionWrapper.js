c9597bc3c8eaa8b5f66f2df3559f975832b0a60e
svtxvt <svetoocvet@gmail.com>
2023-10-07 01:28:10 +0300

    add eslint

--- connectionWrapper.js
+++ connectionWrapper.js
@@ -7,119 +7,114 @@ let globalConnectionCount = 0;
  * TikTok LIVE connection wrapper with advanced reconnect functionality and error handling
  */
 class TikTokConnectionWrapper extends EventEmitter {
-    constructor(uniqueId, options, enableLog) {
-        super();
+  constructor(uniqueId, options, enableLog) {
+    super();
 
-        this.uniqueId = uniqueId;
-        this.enableLog = enableLog;
+    this.uniqueId = uniqueId;
+    this.enableLog = enableLog;
 
-        // Connection State
-        this.clientDisconnected = false;
-        this.reconnectEnabled = true;
-        this.reconnectCount = 0;
-        this.reconnectWaitMs = 1000;
-        this.maxReconnectAttempts = 5;
+    // Connection State
+    this.clientDisconnected = false;
+    this.reconnectEnabled = true;
+    this.reconnectCount = 0;
+    this.reconnectWaitMs = 1000;
+    this.maxReconnectAttempts = 5;
 
-        this.connection = new WebcastPushConnection(uniqueId, options);
+    this.connection = new WebcastPushConnection(uniqueId, options);
 
-        this.connection.on('streamEnd', () => {
-            this.log(`streamEnd event received, giving up connection`);
-            this.reconnectEnabled = false;
-        })
+    this.connection.on('streamEnd', () => {
+      this.log('streamEnd event received, giving up connection');
+      this.reconnectEnabled = false;
+    });
 
-        this.connection.on('disconnected', () => {
-            globalConnectionCount -= 1;
-            this.log(`TikTok connection disconnected`);
-            this.scheduleReconnect();
-        });
+    this.connection.on('disconnected', () => {
+      globalConnectionCount -= 1;
+      this.log('TikTok connection disconnected');
+      this.scheduleReconnect();
+    });
 
-        this.connection.on('error', (err) => {
-            this.log(`Error event triggered: ${err.info}, ${err.exception}`);
-            console.error(err);
-        })
+    this.connection.on('error', (err) => {
+      this.log(`Error event triggered: ${err.info}, ${err.exception}`);
+      console.error(err);
+    });
+  }
+
+  connect(isReconnect) {
+    this.connection.connect().then((state) => {
+      this.log(`${isReconnect ? 'Reconnected' : 'Connected'} to roomId ${state.roomId}, websocket: ${state.upgradedToWebsocket}`);
+
+      globalConnectionCount += 1;
+
+      // Reset reconnect vars
+      this.reconnectCount = 0;
+      this.reconnectWaitMs = 1000;
+
+      // Client disconnected while establishing connection => drop connection
+      if (this.clientDisconnected) {
+        this.connection.disconnect();
+        return;
+      }
+
+      // Notify client
+      if (!isReconnect) {
+        this.emit('connected', state);
+      }
+    }).catch((err) => {
+      this.log(`${isReconnect ? 'Reconnect' : 'Connection'} failed, ${err}`);
+
+      if (isReconnect) {
+        // Schedule the next reconnect attempt
+        this.scheduleReconnect(err);
+      } else {
+        // Notify client
+        this.emit('disconnected', err.toString());
+      }
+    });
+  }
+
+  scheduleReconnect(reason) {
+    if (!this.reconnectEnabled) {
+      return;
     }
 
-    connect(isReconnect) {
-        this.connection.connect().then((state) => {
-            this.log(`${isReconnect ? 'Reconnected' : 'Connected'} to roomId ${state.roomId}, websocket: ${state.upgradedToWebsocket}`);
-
-            globalConnectionCount += 1;
-
-            // Reset reconnect vars
-            this.reconnectCount = 0;
-            this.reconnectWaitMs = 1000;
-
-            // Client disconnected while establishing connection => drop connection
-            if (this.clientDisconnected) {
-                this.connection.disconnect();
-                return;
-            }
-
-            // Notify client
-            if (!isReconnect) {
-                this.emit('connected', state);
-            }
-
-        }).catch((err) => {
-            this.log(`${isReconnect ? 'Reconnect' : 'Connection'} failed, ${err}`);
-
-            if (isReconnect) {
-                // Schedule the next reconnect attempt
-                this.scheduleReconnect(err);
-            } else {
-                // Notify client
-                this.emit('disconnected', err.toString());
-            }
-        })
+    if (this.reconnectCount >= this.maxReconnectAttempts) {
+      this.log('Give up connection, max reconnect attempts exceeded');
+      this.emit('disconnected', `Connection lost. ${reason}`);
+      return;
     }
 
-    scheduleReconnect(reason) {
+    this.log(`Try reconnect in ${this.reconnectWaitMs}ms`);
 
-        if (!this.reconnectEnabled) {
-            return;
-        }
+    setTimeout(() => {
+      if (!this.reconnectEnabled || this.reconnectCount >= this.maxReconnectAttempts) {
+        return;
+      }
 
-        if (this.reconnectCount >= this.maxReconnectAttempts) {
-            this.log(`Give up connection, max reconnect attempts exceeded`);
-            this.emit('disconnected', `Connection lost. ${reason}`);
-            return;
-        }
+      this.reconnectCount += 1;
+      this.reconnectWaitMs *= 2;
+      this.connect(true);
+    }, this.reconnectWaitMs);
+  }
 
-        this.log(`Try reconnect in ${this.reconnectWaitMs}ms`);
+  disconnect() {
+    this.log('Client connection disconnected');
 
-        setTimeout(() => {
-            if (!this.reconnectEnabled || this.reconnectCount >= this.maxReconnectAttempts) {
-                return;
-            }
+    this.clientDisconnected = true;
+    this.reconnectEnabled = false;
 
-            this.reconnectCount += 1;
-            this.reconnectWaitMs *= 2;
-            this.connect(true);
-
-        }, this.reconnectWaitMs)
+    if (this.connection.getState().isConnected) {
+      this.connection.disconnect();
     }
+  }
 
-    disconnect() {
-        this.log(`Client connection disconnected`);
-
-        this.clientDisconnected = true;
-        this.reconnectEnabled = false;
-
-        if (this.connection.getState().isConnected) {
-            this.connection.disconnect();
-        }
+  log(logString) {
+    if (this.enableLog) {
+      console.log(`WRAPPER @${this.uniqueId}: ${logString}`);
     }
-
-    log(logString) {
-        if (this.enableLog) {
-            console.log(`WRAPPER @${this.uniqueId}: ${logString}`);
-        }
-    }
+  }
 }
 
 module.exports = {
-    TikTokConnectionWrapper,
-    getGlobalConnectionCount: () => {
-        return globalConnectionCount;
-    }
+  TikTokConnectionWrapper,
+  getGlobalConnectionCount: () => globalConnectionCount,
 };
--- server.js
+++ server.js
@@ -3,147 +3,145 @@ require('dotenv').config();
 const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { TikTokConnectionWrapper, getGlobalConnectionCount } = require('./connectionWrapper');
 const { clientBlocked } = require('./limiter');
-const {GoogleSpreadsheet}= require("google-spreadsheet");
 
 const app = express();
 const httpServer = createServer(app);
 
 // Enable cross origin resource sharing
 const io = new Server(httpServer, {
-    cors: {
-        origin: '*'
-    }
+  cors: {
+    origin: '*',
+  },
 });
 
-
 io.on('connection', async (socket) => {
-    let tiktokConnectionWrapper;
-    let spreadsheetId;
-    let username;
+  let tiktokConnectionWrapper;
+  let spreadsheetId;
+  let username;
 
-    console.info('New connection from origin', socket.handshake.headers['origin'] || socket.handshake.headers['referer']);
+  console.info('New connection from origin', socket.handshake.headers.origin || socket.handshake.headers.referer);
 
-    socket.on('setUniqueId', async (uniqueId, options) => {
+  socket.on('setUniqueId', async (uniqueId, options) => {
+    // Prohibit the client from specifying these options (for security reasons)
+    if (options && options.spreadsheetId) {
+      delete options.requestOptions;
+      delete options.websocketOptions;
+      spreadsheetId = options.spreadsheetId;
+      username = uniqueId;
+      delete options.spreadsheetId;
+    } else {
+      options = {};
+    }
 
-        // Prohibit the client from specifying these options (for security reasons)
-        if (options && options.spreadsheetId) {
-            delete options.requestOptions;
-            delete options.websocketOptions;
-            spreadsheetId = options.spreadsheetId;
-            username = uniqueId;
-            delete options.spreadsheetId;
-        } else {
-            options = {};
-        }
+    // Session ID in .env file is optional
+    if (process.env.SESSIONID) {
+      options.sessionId = process.env.SESSIONID;
+      console.info('Using SessionId');
+    }
 
-        // Session ID in .env file is optional
-        if (process.env.SESSIONID) {
-            options.sessionId = process.env.SESSIONID;
-            console.info('Using SessionId');
-        }
+    // Check if rate limit exceeded
+    if (process.env.ENABLE_RATE_LIMIT && clientBlocked(io, socket)) {
+      socket.emit('tiktokDisconnected', 'You have opened too many connections or made too many connection requests. Please reduce the number of connections/requests or host your own server instance. The connections are limited to avoid that the server IP gets blocked by TokTok.');
+      return;
+    }
 
-        // Check if rate limit exceeded
-        if (process.env.ENABLE_RATE_LIMIT && clientBlocked(io, socket)) {
-            socket.emit('tiktokDisconnected', 'You have opened too many connections or made too many connection requests. Please reduce the number of connections/requests or host your own server instance. The connections are limited to avoid that the server IP gets blocked by TokTok.');
-            return;
-        }
+    // Connect to the given username (uniqueId)
+    try {
+      tiktokConnectionWrapper = new TikTokConnectionWrapper(uniqueId, options, true);
+      tiktokConnectionWrapper.connect();
+    } catch (err) {
+      socket.emit('tiktokDisconnected', err.toString());
+      return;
+    }
 
-        // Connect to the given username (uniqueId)
+    // Redirect wrapper control events once
+    tiktokConnectionWrapper.once('connected', (state) => socket.emit('tiktokConnected', state));
+    tiktokConnectionWrapper.once('disconnected', (reason) => socket.emit('tiktokDisconnected', reason));
+
+    // Notify client when stream ends
+    tiktokConnectionWrapper.connection.on('streamEnd', () => socket.emit('streamEnd'));
+
+    // Redirect message events
+    tiktokConnectionWrapper.connection.on('roomUser', (msg) => socket.emit('roomUser', msg));
+    tiktokConnectionWrapper.connection.on('member', (msg) => socket.emit('member', msg));
+    tiktokConnectionWrapper.connection.on('chat', async (msg) => {
+      socket.emit('chat', msg);
+
+      // Write the new message to the Google Spreadsheet
+      if (spreadsheetId) {
         try {
-            tiktokConnectionWrapper = new TikTokConnectionWrapper(uniqueId, options, true);
-            tiktokConnectionWrapper.connect();
-        } catch (err) {
-            socket.emit('tiktokDisconnected', err.toString());
-            return;
+          const doc = new GoogleSpreadsheet(spreadsheetId);
+          const config = {
+            type: process.env.GOOGLE_ACCOUNT_TYPE,
+            project_id: process.env.GOOGLE_PROJECT_ID,
+            private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+            private_key: process.env.GOOGLE_PRIVATE_KEY.split(String.raw`\n`).join('\n'),
+            client_email: process.env.GOOGLE_CLIENT_EMAIL,
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            auth_uri: process.env.GOOGLE_AUTH_URI,
+            token_uri: process.env.GOOGLE_TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+            client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+          };
+
+          await doc?.useServiceAccountAuth(config, 'oleksandra-kalinina@landing-page-creator-378916.iam.gserviceaccount.com');
+          await doc?.loadInfo();
+          let sheet;
+          sheet = doc?.sheetsByTitle[username];
+          console.log('username', username);
+          if (!sheet) {
+            sheet = await doc.addSheet({ title: username, headerValues: ['social_network', 'author', 'message'] });
+          }
+          await sheet?.addRow({
+            social_network: 'TikTok',
+            author: msg.uniqueId,
+            message: msg.comment,
+          });
+        } catch (error) {
+          console.error('Error fetching context from Google Sheet:', JSON.stringify(error));
         }
-
-        // Redirect wrapper control events once
-        tiktokConnectionWrapper.once('connected', state => socket.emit('tiktokConnected', state));
-        tiktokConnectionWrapper.once('disconnected', reason => socket.emit('tiktokDisconnected', reason));
-
-        // Notify client when stream ends
-        tiktokConnectionWrapper.connection.on('streamEnd', () => socket.emit('streamEnd'));
-
-        // Redirect message events
-        tiktokConnectionWrapper.connection.on('roomUser', msg => socket.emit('roomUser', msg));
-        tiktokConnectionWrapper.connection.on('member', msg => socket.emit('member', msg));
-        tiktokConnectionWrapper.connection.on('chat', async (msg) => {
-            socket.emit('chat', msg);
-
-            // Write the new message to the Google Spreadsheet
-            if (spreadsheetId) {
-                try {
-                const doc = new GoogleSpreadsheet(spreadsheetId);
-                const config = {
-                    "type": process.env.GOOGLE_ACCOUNT_TYPE,
-                    "project_id": process.env.GOOGLE_PROJECT_ID,
-                    "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
-                    "private_key": process.env.GOOGLE_PRIVATE_KEY.split(String.raw`\n`).join('\n'),
-                    "client_email": process.env.GOOGLE_CLIENT_EMAIL,
-                    "client_id": process.env.GOOGLE_CLIENT_ID,
-                    "auth_uri": process.env.GOOGLE_AUTH_URI,
-                    "token_uri": process.env.GOOGLE_TOKEN_URI,
-                    "auth_provider_x509_cert_url": process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-                    "client_x509_cert_url": process.env.GOOGLE_CLIENT_X509_CERT_URL
-                }
-
-                await doc?.useServiceAccountAuth(config, 'oleksandra-kalinina@landing-page-creator-378916.iam.gserviceaccount.com');
-                await doc?.loadInfo();
-                let sheet;
-                    sheet = doc?.sheetsByTitle[username];
-                    console.log('username', username);
-                    if (!sheet) {
-                       sheet = await doc.addSheet({ title: username, headerValues: ['social_network', 'author', 'message'] });
-                    }
-                    await sheet?.addRow({
-                        'social_network': 'TikTok',
-                        'author': msg.uniqueId,
-                        'message': msg.comment,
-                    });
-                } catch (error) {
-                    console.error("Error fetching context from Google Sheet:", JSON.stringify(error));
-                }
-            }
-        });
-        tiktokConnectionWrapper.connection.on('gift', msg => socket.emit('gift', msg));
-        tiktokConnectionWrapper.connection.on('social', msg => socket.emit('social', msg));
-        // tiktokConnectionWrapper.connection.on('like', msg => socket.emit('like', msg));
-        tiktokConnectionWrapper.connection.on('questionNew', msg => socket.emit('questionNew', msg));
-        tiktokConnectionWrapper.connection.on('linkMicBattle', msg => socket.emit('linkMicBattle', msg));
-        tiktokConnectionWrapper.connection.on('linkMicArmies', msg => socket.emit('linkMicArmies', msg));
-        tiktokConnectionWrapper.connection.on('liveIntro', msg => socket.emit('liveIntro', msg));
-        tiktokConnectionWrapper.connection.on('emote', msg => socket.emit('emote', msg));
-        tiktokConnectionWrapper.connection.on('envelope', msg => socket.emit('envelope', msg));
-        tiktokConnectionWrapper.connection.on('subscribe', msg => socket.emit('subscribe', msg));
+      }
     });
+    tiktokConnectionWrapper.connection.on('gift', (msg) => socket.emit('gift', msg));
+    tiktokConnectionWrapper.connection.on('social', (msg) => socket.emit('social', msg));
+    // tiktokConnectionWrapper.connection.on('like', msg => socket.emit('like', msg));
+    tiktokConnectionWrapper.connection.on('questionNew', (msg) => socket.emit('questionNew', msg));
+    tiktokConnectionWrapper.connection.on('linkMicBattle', (msg) => socket.emit('linkMicBattle', msg));
+    tiktokConnectionWrapper.connection.on('linkMicArmies', (msg) => socket.emit('linkMicArmies', msg));
+    tiktokConnectionWrapper.connection.on('liveIntro', (msg) => socket.emit('liveIntro', msg));
+    tiktokConnectionWrapper.connection.on('emote', (msg) => socket.emit('emote', msg));
+    tiktokConnectionWrapper.connection.on('envelope', (msg) => socket.emit('envelope', msg));
+    tiktokConnectionWrapper.connection.on('subscribe', (msg) => socket.emit('subscribe', msg));
+  });
 
-    socket.on('disconnect', () => {
-        if (tiktokConnectionWrapper) {
-            tiktokConnectionWrapper.disconnect();
-        }
-    });
+  socket.on('disconnect', () => {
+    if (tiktokConnectionWrapper) {
+      tiktokConnectionWrapper.disconnect();
+    }
+  });
 });
 
 // Emit global connection statistics
 setInterval(() => {
-    io.emit('statistic', { globalConnectionCount: getGlobalConnectionCount() });
-}, 5000)
+  io.emit('statistic', { globalConnectionCount: getGlobalConnectionCount() });
+}, 5000);
 
-//base auth
+// base auth
 app.use((req, res, next) => {
-    const auth = { login: process.env.USER, password:process.env.PASS };
+  const auth = { login: process.env.USER, password: process.env.PASS };
 
-    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
-    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
 
-    if (login && login === auth.login && password && password === auth.password) {
-        return next();
-    }
+  if (login && login === auth.login && password && password === auth.password) {
+    return next();
+  }
 
-    res.set('WWW-Authenticate', 'Basic realm="401"');
-    res.status(401).send('Authentication required.');
+  res.set('WWW-Authenticate', 'Basic realm="401"');
+  res.status(401).send('Authentication required.');
 });
 
 // Serve frontend files
